c272b01fbd4b11aed57335c4a98bda65d32f75ac
PavelKharitonov <ecclesiastes@tut.by>
2020-03-21 17:13:08 +0300

    push function is exception in typescript

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ function add(seed, num1, num2) {
     return seed + num1 + num2;
 }
 var seed;
+var mixedArray = [0, 'asdasd'];
+mixedArray.push('pss');
 button.addEventListener('click', function () {
     seed = '5';
     console.log(add(seed, +input1.value, +input2.value));
+    console.log(mixedArray);
 });
--- index.ts
+++ index.ts
@@ -3,13 +3,16 @@ const input1 = document.getElementById('input1')! as HTMLInputElement;
 const input2 = document.getElementById('input2')! as HTMLInputElement;
 const output = document.getElementById('output');
 
-function add(seed: number, num1: number, num2: number) {
+function add(seed: any, num1: number, num2: number) {
     return seed + num1 + num2;
 }
 
-let seed;
+let seed: (string | number);
+let mixedArray: [number, string] = [0, 'asdasd'];
+mixedArray.push('pss');
 
 button.addEventListener('click', function () {
     seed = '5';
     console.log(add(seed, +input1.value, +input2.value));
+    console.log(mixedArray);
 });
