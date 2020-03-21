66712c682b5a402f70d71923806d611717a603b2
PavelKharitonov <ecclesiastes@tut.by>
2020-03-21 16:37:13 +0300

    ts does not check all variable assignments

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var button = document.getElementById('btn');
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
 var output = document.getElementById('output');
-function add(num1, num2) {
-    return num1 + num2;
+function add(seed, num1, num2) {
+    return seed + num1 + num2;
 }
+var seed;
 button.addEventListener('click', function () {
-    console.log(add(+input1.value, +input2.value));
-    console.log('pasha');
+    seed = '5';
+    console.log(add(seed, +input1.value, +input2.value));
 });
--- index.ts
+++ index.ts
@@ -3,10 +3,13 @@ const input1 = document.getElementById('input1')! as HTMLInputElement;
 const input2 = document.getElementById('input2')! as HTMLInputElement;
 const output = document.getElementById('output');
 
-function add(num1: number, num2: number) {
-    return num1 + num2;
+function add(seed: number, num1: number, num2: number) {
+    return seed + num1 + num2;
 }
 
+let seed;
+
 button.addEventListener('click', function () {
-    console.log(add(+input1.value, +input2.value));
+    seed = '5';
+    console.log(add(seed, +input1.value, +input2.value));
 });
